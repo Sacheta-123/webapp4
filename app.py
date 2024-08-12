8c9f812d7ccec4ca289a91522ac3b74f4cf85bef
imraj <166581608+imraj0397@users.noreply.github.com>
2024-08-12 19:16:58 +0530

    new update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Vanakkam Da Mappala, Medavakkathula irundhu 😎🤠😈"
+    return "Vanakkam Da Mappla, Medavakkathula irundhu 😎🤠😈"
 
 @app.route('/how-are-you')
 def how_are_you():
