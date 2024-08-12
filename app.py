a8721f1ae314e5998dfcf83c1ac4510e18fc30b8
imraj <166581608+imraj0397@users.noreply.github.com>
2024-08-12 18:51:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 def main():
     return "Welcome to webhooks!"
 
-@app.route('/how are you')
-def hello():
+@app.route('/how-are-you')
+def how_are_you():
     return 'I am good, how about you? This is my new update to the app.py to test the OpenShift Webhooks'
 
 @app.route('/test')
-def hello():
+def test():
     return "Well, the test works!!!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
