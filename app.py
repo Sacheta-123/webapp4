9fa194cabe7e055047b14cdb3480b0c4602d4692
denisardika <denisagustian735@gmail.com>
2025-06-27 05:51:27 +0000

    Tambah halaman /about

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return "Welcome!"
 
-@app.route('/how are you')
+@app.route("/how are you")
 def hello():
-    return 'I am good, how about you?'
+    return "I am good, how about you?"
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1><p>This is a simple Flask app for testing PR.</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
