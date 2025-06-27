3a68d910181444e9c5243a4265c7199478bd59d1
iqbalysnta <iqbalysnta@gmail.com>
2025-06-27 03:29:15 +0000

    feat: tambah route /about

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return "Welcome!"
+def home():
+    return "Hello, World!"
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
+@app.route("/about")
+def about():
+    return "Ini halaman About"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
